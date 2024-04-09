32aa38eba6f1880f692707f0d0a02a466f2ce415
Josh <joshbhorn@gmail.com>
2024-04-09 15:21:47 -0500

    created user serializer and CreateAcoount view

--- backend/userapp/urls.py
+++ backend/userapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('create-account/', CreateAccount.as_view(), name='createaccount'),
 ]
--- backend/userapp/views.py
+++ backend/userapp/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import authenticate
+from .models import User
+from .serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Q
+from rest_framework.status import (
+    HTTP_201_CREATED,
+    HTTP_404_NOT_FOUND,
+    HTTP_204_NO_CONTENT,
+    HTTP_400_BAD_REQUEST,
+    HTTP_200_OK,
+)
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import LimitOffsetPagination
+from datetime import datetime, timedelta
+# from .utilities import HttpOnlyToken
 
-# Create your views here.
+class CreateAccount(APIView):
+
+    def post(self, request):
+
+        try:
+            user = User.objects.create_user(
+                username = request.data["username"]
+            )
+            token = Token.objects.create(user = user)
+            return Response(
+                {"user": user.username, "token": token.key}, status= HTTP_201_CREATED
+            )
+        except:
+            return Response(
+                {"error": "A user with this username already exists."}, status= HTTP_400_BAD_REQUEST
+            )
