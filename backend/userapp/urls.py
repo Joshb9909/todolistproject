4322ad1bc0ee1ca2d0bb1167889842e97e1daeb8
Josh <joshbhorn@gmail.com>
2024-04-12 20:20:36 -0500

    created urls and views for log out and delete account and created utilities to handle user authentification on requests.

--- backend/userapp/urls.py
+++ backend/userapp/urls.py
@@ -3,4 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('create-account/', CreateAccount.as_view(), name='createaccount'),
+    path('login/', LogIn.as_view(), name="login"),
+    path('logout/', LogOut.as_view(), name="logout"),
+    path('deleteaccount/', DeleteAccount.as_view(), name="deleteaccount"),
 ]
--- backend/userapp/views.py
+++ backend/userapp/views.py
@@ -17,7 +17,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import LimitOffsetPagination
 from datetime import datetime, timedelta
-# from .utilities import HttpOnlyToken
+from .utilities import HttpOnlyToken
 
 class CreateAccount(APIView):
 
@@ -36,7 +36,7 @@ class CreateAccount(APIView):
                 {"error": "A user with this username already exists."}, status= HTTP_400_BAD_REQUEST
             )
 
-class Log_in(APIView):
+class LogIn(APIView):
     def post(self, request):
         username = request.data.get("username")
         password = request.data.get("password")
@@ -52,3 +52,21 @@ class Log_in(APIView):
             # return Response({"token": token.key, **user_serializer.data})
         else:
             return Response("No user matching credentials", status=HTTP_404_NOT_FOUND)
+        
+class LogOut(APIView):
+    authentication_classes = [HttpOnlyToken]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+    
+class DeleteAccount(APIView):
+    authentication_classes = [HttpOnlyToken]
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request):
+
+        request.user.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+
