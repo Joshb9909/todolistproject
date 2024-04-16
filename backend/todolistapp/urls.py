be44470160d4955456aac75474e4f23a4f6b18bb
Josh <joshbhorn@gmail.com>
2024-04-16 14:02:43 -0500

    created view and url for DeleteToDoList

--- backend/todolistapp/urls.py
+++ backend/todolistapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('create_list/', CreateToDoList.as_view(), name='createlist')
+    path('create_list/', CreateToDoList.as_view(), name='createlist'),
+    path('delete_list/', DeleteToDoList.as_view(), name='deletelist'),
 ]
--- backend/todolistapp/views.py
+++ backend/todolistapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import toDoList
+from .models import ToDoList
 from .serializers import ToDoListSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -9,6 +9,7 @@ from rest_framework.status import (
     HTTP_204_NO_CONTENT,
     HTTP_400_BAD_REQUEST,
     HTTP_200_OK,
+    HTTP_403_FORBIDDEN
 )
 from userapp.utilities import HttpOnlyToken
 from rest_framework.permissions import IsAuthenticated
@@ -32,4 +33,19 @@ class CreateToDoList(APIView):
         else:
             return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
+class DeleteToDoList(APIView):
+    authentication_classes = [HttpOnlyToken]
+    permission_classes = [IsAuthenticated]
 
+    def delete(self, request):
+
+        to_do_list_id = request.data.get('list_id')
+        to_do_list = get_object_or_404(ToDoList, pk=to_do_list_id)
+
+        if request.user != to_do_list.user_id:
+
+            return Response(status= HTTP_403_FORBIDDEN)
+        
+        to_do_list.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+    
