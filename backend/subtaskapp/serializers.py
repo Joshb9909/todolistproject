d4ae6702c01314005064482b3021c177fd7f286b
Josh <joshbhorn@gmail.com>
2024-05-15 15:45:39 -0500

    created subtask serializer

---
+++ backend/subtaskapp/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import SubTask
+
+class SubTaskSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = SubTask
+        fields = ['task_id', 'sub_task_text']
--- backend/subtaskapp/views.py
+++ backend/subtaskapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import SubTask
+from .serializers import SubTaskSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_201_CREATED,
+    HTTP_404_NOT_FOUND,
+    HTTP_204_NO_CONTENT,
+    HTTP_400_BAD_REQUEST,
+    HTTP_200_OK,
+    HTTP_403_FORBIDDEN
+)
+from userapp.utilities import HttpOnlyToken
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
